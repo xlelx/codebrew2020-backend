e29273784c2aa647a83ecf6a11b46709b1d2ba3e
Dian Lin <dilin@student.unimelb.edu.au>
2020-09-27 05:49:07 +1000

    add post methods for prescription

--- functions/handlers/medications.js
+++ functions/handlers/medications.js
@@ -20,3 +20,57 @@ exports.getAllMedications = (req, res) => {
             return res.status(500).json({ error: err.code });
         })
 }
+
+exports.getAllRestrictions = (req, res) => {
+    medicationsRef
+        .where('patientId', '==', req.user.id)
+        .get()
+        .then(data => {
+            let restrictions = [];
+            data.docs.map((doc) => {
+                restrictions.push({
+                    medicationId: doc.id,
+                    source: doc.medicationName,
+                    description: doc.restrictions
+                })
+            })
+            return res.json(restrictions);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+}
+
+exports.postMedication = (medication, prescriptionId, patientId) => {
+
+    const newMedication = {
+        patientId: patientId,
+        name: medication.medicationName,
+        prescriptionId: prescriptionId,
+        startDate: medication.startDate,
+        endDate: medication.endDate,
+        restrictions: medication.restrictions,
+        instructions: medication.instructions
+    }
+
+    // diagnosis should not be empty 
+    if (newMedication.body.name.trim() === '') {
+        res.status(400).json({body: "Should not be empty!"});
+    }
+
+    // if medication name is not empty, create a new prescription document in firestore
+    else {
+        medicationsRef
+        .add(newMedication)
+        .then(doc => {
+            const resMedication = newMedication;
+            resMedication.medicationId = doc.id;
+            res.json(resMedication);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong' });
+        })
+    }
+
+}
--- functions/handlers/prescriptions.js
+++ functions/handlers/prescriptions.js
@@ -1,5 +1,6 @@
 const { db } = require('../util/admin');
 const prescriptionsRef = db.collection('prescriptions');
+const { postMedication } = require('./medications');
 
 exports.getAllPrescriptions = (req, res) => {
     prescriptionsRef
@@ -23,10 +24,52 @@ exports.getAllPrescriptions = (req, res) => {
 }
 
 exports.postPrescription = (req, res) => {
+    const medications = req.body.medications;
+    const restrictions = req.body.warnings;
+
     const newPrescription = {
-        
+        clinic: req.body.clinic,
+        dateExpire: req.body.prescriptionEndDate,
+        dateGenerated: req.body.prescriptionStartDate,
+        patientFirstname: req.body.patientFirstName,
+        patientLastName: req.body.patientLastName,
+        patientId: req.body.patientId,
+        diagnosis: req.body.diagnosis,
+        doctorId: req.body.doctorId
     }
+
+    // diagnosis should not be empty 
+    if (newPrescription.body.diagnosis.trim() === '') {
+        res.status(400).json({body: "Should not be empty!"});
+    }
+
+    // if diagnosis is not empty, create a new prescription document in firestore
+    else {
+        prescriptionsRef
+        .add(newPrescription)
+        .then(doc => {
+            const resPrescription = newPrescription;
+            resPrescription.prescriptionId = doc.id;
+
+            // ADD THE MEDICATIONS FROM THIS PRESCRIPTION TO DATABASE
+            for (var i=0; i<medications.length; i++) {
+                postMedication(medication, resPrescription.prescriptionId, req.body.patientId);
+            }
+
+            // ADD THE WARNING FROM THIS PRESCRIPTION TO DATABASE if any
+            
+            
+            
+            res.json(resPrescription);
+            
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong' });
+        })
+    }
+
 }
+
 
 
 
